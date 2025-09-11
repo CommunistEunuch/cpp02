ebe91edfbc2ab58a198a6f53b2fcc801db770d73
medicaless <danil00@naver.com>
2025-09-11 11:27:58 +0900

    week03 v0.2 virtual

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// 베이스 클래스의 정의
 class Pokemon
 {
 public:
-	virtual void attack() const { cout << "공격 !" << endl; }
+	virtual void attack() const { cout << "공격 !" << endl; } //3. virtual 함수로 생성
 };
-// 파생 클래스의 정의
 class Pikachu : public Pokemon //is-a
 {
 public:
-	virtual void attack() const { cout << "Electric Ball" << endl; }
+	virtual void attack() const { cout << "Electric Ball" << endl; } //3. virtual 함수로 생성
 };
 int main()
 {
-	Pokemon* pokemon;
-	pokemon = new Pokemon();
+	Pokemon* pokemon; //1. 포인터 변수
+	
+	pokemon = new Pokemon(); //2. 객체 호환 가능
 	pokemon->attack();
 	delete pokemon;
-	pokemon = new Pikachu();
+
+	pokemon = new Pikachu(); //2. 객체 호환 가능
 	pokemon->attack();
 	delete pokemon;
+	
 	return 0;
 }
