51846003a2b4767b83cdc0e8e368f28dac7c4a61
medicaless <danil00@naver.com>
2025-09-25 11:39:53 +0900

    week05 safe casting (dynamic cast)

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -28,7 +28,10 @@ int main()
 	pa->makeSound();
 	cout << pa << endl;
 
-	Cat* pc = (Cat*)pa; //Dangerous behavior, And Run smoothly, terrible
+	//Cat* pc = (Cat*)pa; //Dangerous behavior, And Run smoothly, terrible
+	//Cat* pc = dynamic_cast<Cat*>(pa); //Safe. 고양이클래스의 포인터가 강아지 클래스의 메모리 번지주소를 받을 수 없다(null)
+	Dog* pc = dynamic_cast<Dog*>(pa); //힙에 할당된 강아지클래스 객체의 메모리 번지 주소를 받는다.
+
 	pc->makeSound();
 	cout << pc << endl;
 	delete pc;
