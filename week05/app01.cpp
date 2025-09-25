70d8a349e2bd556563d673460afae92cf193b79a
medicaless <danil00@naver.com>
2025-09-25 17:51:06 +0900

    week07 multiple inheritance added

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -5,9 +5,7 @@ using namespace std;
 
 class Animal {
 public:
-	virtual void makeSound() {
-		cout << "cry \n";
-	};
+	virtual void makeSound()=0;
 };
 class Dog : public Animal {
 public:
@@ -24,27 +22,9 @@ public:
 
 int main()
 {
-	Animal* pa = new Dog(); //up-casting
-	pa->makeSound();
-	cout << pa << endl;
+	//Animal animal;
+	//animal.makeSound();
 
-	//Cat* pc = (Cat*)pa; //Dangerous behavior, And Run smoothly, terrible
-	//Cat* pc = dynamic_cast<Cat*>(pa); //Safe. 고양이클래스의 포인터가 강아지 클래스의 메모리 번지주소를 받을 수 없다(null)
-	Dog* pc = dynamic_cast<Dog*>(pa); //힙에 할당된 강아지클래스 객체의 메모리 번지 주소를 받는다.
-
-	pc->makeSound();
-	cout << pc << endl;
-	delete pc;
-	pc = nullptr;
-
-	//Dog* pd = (Dog*)pa; //down-casting, old style (C style)
-	//Dog* pd = dynamic_cast<Dog*>(pa); //dynamic-cast
-	//pd->makeSound();
-	//delete pa;
-	//pa = nullptr;
-
-
-
-
+	Animal animal; //순수 가상함수
 	return 0;
 }
