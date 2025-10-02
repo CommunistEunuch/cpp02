6fa263de05e3ed9bc3544e4692c9a81a86ee753b
medicaless <danil00@naver.com>
2025-10-02 11:33:35 +0900

    week06 operator prefix postfix overloading

--- week06/app01.cpp
+++ week06/app01.cpp
@@ -6,10 +6,10 @@ private:
 	int real;
 	int imaginary;
 
-//public:
-//	void setReal(int real) : rela(real){
-//		this->real = real;
-//	} //당연히 안되지 이 양반아
+	//public:
+	//	void setReal(int real) : rela(real){
+	//		this->real = real;
+	//	} //당연히 안되지 이 양반아
 
 public:
 	Complex() {}
@@ -31,25 +31,43 @@ public:
 		int r = this->real + right.real;
 		int i = this->imaginary + right.imaginary;
 
-		return Complex(r,i);
+		return Complex(r, i);
+		//return Complex(r, this->imaginary) // 가능하지만 수학적으로 잘못됨.
+	}
+
+	Complex operator++(int) { //tag : c3.real+1 (피연산자가 2개가 아닌 1개라서 명시해야함.) (후위연산)
+		Complex previous(this->real, this->imaginary);
+		this->real = this->real + 1;
+		return previous;
+	}
+
+	Complex operator++() { //전위연산
+		this->real++;
+		return Complex(this->real, this->imaginary);
 	}
 };
 
 int main() {
-	Complex c1, c2;
-	//Complex c1(5, 3);
+	Complex c1;
+	Complex c2(10, 7);
 
 	c1.setReal(5);
 	c1.setImaginary(3);
-	c2.setReal(10);
-	c2.setImaginary(7);
 
 	cout << c1.getReal() << "+" << c1.getImaginary() << "i" << endl;
 	cout << c2.getReal() << "+" << c2.getImaginary() << "i" << endl;
 	//Complex c3 = c1 + c2; //이걸 어케 계산해요 주인님
 
-	Complex c3 = c1 + c2; 
+	Complex c3 = c1 + c2; // Complex c3 = c1.operator+(c2); 
 	cout << c3.getReal() << "+" << c3.getImaginary() << "i" << endl;
+
+	Complex c4 = c3++;
+	Complex c5 = ++c3;
+
+	//c3++; //real값만 1 증가
+	cout << c3.getReal() << "+" << c3.getImaginary() << "i" << endl;
+	cout << c4.getReal() << "+" << c4.getImaginary() << "i" << endl;
+	cout << c5.getReal() << "+" << c5.getImaginary() << "i" << endl;
 
 	return 0;
 }
