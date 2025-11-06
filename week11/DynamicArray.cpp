b3e98266c3a12de294c15876b7d561c5af98a90b
medicaless <danil00@naver.com>
2025-11-06 11:11:19 +0900

    week11 modified

--- week11/DynamicArray.cpp
+++ week11/DynamicArray.cpp
@@ -17,3 +17,14 @@ DynamicArray::~DynamicArray()
 	cout << "동적배열 해제" << endl;
 	delete[]ptr;
 }
+
+int DynamicArray::getAt(int index)
+{
+	return ptr[index];
+}
+
+void DynamicArray::setAt(int index, int value)
+{
+	ptr[index] = value;
+
+}
